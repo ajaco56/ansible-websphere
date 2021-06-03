ab0dd84e8aaf60781e933eb01a1747d063bae920
ajaco56 <50524874+ajaco56@users.noreply.github.com>
2021-06-03 21:54:35 +0200

    Update liberty_server.py

--- examples/library/liberty_server.py
+++ examples/library/liberty_server.py
@@ -21,7 +21,7 @@ def main():
     )
 
     state = module.params['state']
-    name = module.params['name']
+    servername = module.params['servername']
     libertydir = module.params['libertydir']
 
     # Check if paths are valid
@@ -29,22 +29,22 @@ def main():
      #   module.fail_json(msg=libertydir+" does not exists")
 
     if state == 'stopped':
-        child = subprocess.Popen([libertydir+"/bin/server stop " + name], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        child = subprocess.Popen([libertydir+"/bin/server stop " + servername], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout_value, stderr_value = child.communicate()
         if child.returncode != 0:
-           #if not stderr_value.find(b"is not running") != -1:
+           if not stderr_value.find(b"is not running") != -1:
                module.fail_json(msg=name + " stop failed", stdout=stdout_value, stderr=stderr_value)
 
         module.exit_json(changed=True, msg=name + " stopped successfully", stdout=stdout_value)
 
     if state == 'started':
-        child = subprocess.Popen([libertydir+"/bin/server start " + name], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        child = subprocess.Popen([libertydir+"/bin/server start " + servername], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout_value, stderr_value = child.communicate()
         if child.returncode != 0:
-           #if not stderr_value.find(b"is running with process") != -1:
+           if not stderr_value.find(b"is running with process") != -1:
                module.fail_json(msg=name + " start failed", stdout=stdout_value, stderr=stderr_value)
 
-        module.exit_json(changed=True, msg=name + " started successfully", stdout=stdout_value)
+        module.exit_json(changed=True, msg=servername + " started successfully", stdout=stdout_value)
 
 
 # import module snippets
