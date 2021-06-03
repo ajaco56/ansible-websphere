a4c7221d457642183e60a142230f757d97c90f74
ajaco56 <50524874+ajaco56@users.noreply.github.com>
2021-06-03 21:58:11 +0200

    Update liberty_server.py

--- examples/library/liberty_server.py
+++ examples/library/liberty_server.py
@@ -33,16 +33,16 @@ def main():
         stdout_value, stderr_value = child.communicate()
         if child.returncode != 0:
            if not stderr_value.find(b"is not running") != -1:
-               module.fail_json(msg=name + " stop failed", stdout=stdout_value, stderr=stderr_value)
+               module.fail_json(msg=servername + " stop failed", stdout=stdout_value, stderr=stderr_value)
 
-        module.exit_json(changed=True, msg=name + " stopped successfully", stdout=stdout_value)
+        module.exit_json(changed=True, msg=servername + " stopped successfully", stdout=stdout_value)
 
     if state == 'started':
         child = subprocess.Popen([libertydir+"/bin/server start " + servername], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout_value, stderr_value = child.communicate()
         if child.returncode != 0:
            if not stderr_value.find(b"is running with process") != -1:
-               module.fail_json(msg=name + " start failed", stdout=stdout_value, stderr=stderr_value)
+               module.fail_json(msg=servername + " start failed", stdout=stdout_value, stderr=stderr_value)
 
         module.exit_json(changed=True, msg=servername + " started successfully", stdout=stdout_value)
 
