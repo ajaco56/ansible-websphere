0cad62e3fc078e2ec0687884d2269abed103f4a4
ajaco56 <50524874+ajaco56@users.noreply.github.com>
2021-06-06 17:48:11 +0200

    Update liberty_server.py

--- examples/library/liberty_server.py
+++ examples/library/liberty_server.py
@@ -54,8 +54,8 @@ def main():
         child = subprocess.Popen([libertydir+"/bin/server status " + servername], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout_value, stderr_value = child.communicate()
         #if child.returncode != 0:
-        #   if stderr_value.find(b"is not running") != -1:
-        #       module.fail_json(msg=servername + " status failed", stdout=stdout_value, stderr=stderr_value)
+        if not stdout_value.find(b"does not exist") != -1:
+            module.fail_json(msg=servername + " status failed", stdout=stdout_value, stderr=stderr_value)
         
         #time.sleep(5)
         
